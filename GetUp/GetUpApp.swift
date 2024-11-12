0bb24cdf8274044d7f4e02838cbac86978dd5819
chavalitpramotedham <chavalit@pramotedham.com>
2024-11-13 02:22:24 +0800

    Initial Commit

---
+++ GetUp/GetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetUpApp.swift
+//  GetUp
+//
+//  Created by ByteDance on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GetUpTests/GetUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  GetUpTests.swift
+//  GetUpTests
+//
+//  Created by ByteDance on 13/11/24.
+//
+
+import Testing
+@testable import GetUp
+
+struct GetUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
